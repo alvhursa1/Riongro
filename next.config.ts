66105b9df7479ff75335e589333a506510fa2c26
alvhursa1 <alvaro.hurtado.s@me.com>
2024-10-27 12:12:52 -0500

    v6 271024

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      loaders: {}, // Puedes agregar configuraciones específicas aquí si es necesario
+    },
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+    return config; // Asegúrate de devolver la configuración
   },
 };
 
